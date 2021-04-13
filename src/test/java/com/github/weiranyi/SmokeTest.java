78518855f633cbb4fb0cc9e13e1b8a39673291eb
yiweiran <168578203@qq.com>
2021-04-13 13:21:50 +0800

    使用链表实现一个栈，并测试了性能

--- src/test/java/com/github/weiranyi/SmokeTest.java
+++ src/test/java/com/github/weiranyi/SmokeTest.java
@@ -1,8 +1,12 @@
 package com.github.weiranyi;
 
+import com.github.weiranyi.array.ArrayStack;
 import com.github.weiranyi.linkedList.LinkedList;
 import com.github.weiranyi.linkedList.LinkedListStack;
+import com.github.weiranyi.stacks.Stack;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 public class SmokeTest {
     @Test
@@ -36,5 +40,28 @@ public class SmokeTest {
         linkedListStack.pop();
         System.out.println(linkedListStack);
 
+        int opCount = 100000;
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack1 = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack1, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
+    }
+
+    // 测试使用stack运行opCount个push和pop操作所需要的时间，单位：秒
+    private static double testStack(Stack<Integer> stack, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
     }
 }
